76393c57b4e5b762f4fd68d8a299bbcbff106e5c
cbustamanteaponte <cbustamanteaponte@gmail.com>
2016-05-01 22:17:54 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+// DB connection info
+$host = "aelu.database.windows.net\sqlexpress";
+$user = "aelu";
+$pwd = "$Carlos1234";
+$db = "AELU";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql_select = "SELECT * FROM deporte";
+    $stmt = $conn->query($sql_select);
+    $registrants = $stmt->fetchAll(); 
+    if(count($registrants) > 0) {
+        echo "<h2>People who are registered:</h2>";
+        echo "<table>";
+        echo "<tr><th>Name</th>";
+        echo "<th>Email</th>";
+        echo "<th>Date</th></tr>";
+        foreach($registrants as $registrant) {
+            echo "<tr><td>".$registrant['Nombre']."</td>";
+        }
+        echo "</table>";
+    } else {
+        echo "<h3>No one is currently registered.</h3>";
+    }
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
+?>
